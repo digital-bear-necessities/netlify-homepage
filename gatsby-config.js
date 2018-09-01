97a4009782f1d27836150f5352635bee65966188
Luke Oliff <luke@lukeoliff.com>
2018-09-01 20:00:24 +0100

    trackers

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,23 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-125062080-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-hubspot`,
+      options: {
+        trackingCode: 4880190
+      },
+    },
   ],
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,19 +13,6 @@ const TemplateWrapper = ({ children }) => (
     <Helmet title="Digital Bear Necessities Ltd" />
     {children()}
     <AppFooter />
-    <!-- Start of HubSpot Embed Code -->
-    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4880190.js"></script>
-    <!-- End of HubSpot Embed Code -->
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125062080-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-125062080-1');
-    </script>
-
   </div>
 )
 
